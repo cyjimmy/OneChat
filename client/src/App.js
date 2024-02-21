3d8ca66bc03ad25eb68511732fef40042d060a07
Jimmy Wong <cy.jimmy.wong@gmail.com>
2024-02-21 14:34:40 -0800

    remove unused components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Welcome from "./Components/Welcome";
 import Chat from "./Components/Chat";
 import "./App.css";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/chat" element={<Chat />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return 
+  //TODO: Header component
+  //TODO: Chat page
+  ;
 };
 
 export default App;
